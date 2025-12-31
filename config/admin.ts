25e42d5e6510cd68191b0ecf0118d1de827c5b25
huyphansy226@gmail.com <huyphansy226@gmail.com>
2025-12-31 16:37:25 +0700

    fix bug

--- config/admin.ts
+++ config/admin.ts
@@ -1,6 +1,6 @@
 export default ({ env }) => ({
   auth: {
-    secret: env('ADMIN_AUTH_SECRET'),
+    secret: env('ADMIN_JWT_SECRET'),
   },
   apiToken: {
     salt: env('API_TOKEN_SALT'),
@@ -12,9 +12,5 @@ export default ({ env }) => ({
   },
   secrets: {
     encryptionKey: env('ENCRYPTION_KEY'),
-  },
-  flags: {
-    nps: env.bool('FLAG_NPS', true),
-    promoteEE: env.bool('FLAG_PROMOTE_EE', true),
   },
 });
