20004a29042d1eafaacb1213bc0e54bd4a32ebe1
huyphansy226@gmail.com <huyphansy226@gmail.com>
2025-12-31 16:27:17 +0700

    update

--- config/database.ts
+++ config/database.ts
@@ -7,9 +7,13 @@ export default ({ env }) => ({
       database: env('DATABASE_NAME'),
       user: env('DATABASE_USERNAME'),
       password: env('DATABASE_PASSWORD'),
-      ssl: env.bool('DATABASE_SSL')
-        ? { rejectUnauthorized: false }
-        : false,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
+    pool: {
+      min: 0,
+      max: 10,
     },
     debug: false,
   },
